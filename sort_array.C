0cefe2c8a1cb727a9f3e2979081db65a1a241e49
bharanivsb11 <35220778+bharanivsb11@users.noreply.github.com>
2018-01-31 16:41:47 +0530

    sort_array.C

--- sort_array.C
+++ sort_array.C
@@ -1,14 +1,34 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
+ 
 int main()
 {
-int i,j,temp;
-int size;
-int arr[25];
-printf("enter the size of array");
-scanf("%d",&size);
-for(i=0;i<size;i++)
-{
-scanf("%d",&arr[i]);
+    int arr[25];
+    int size;
+    int i, j, temp;
+    printf("Enter size of array: ");
+    scanf("%d", &size);
+    for(i=0; i<size; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+ 
+    for(i=0; i<size; i++)
+    {
+        for(j=i+1; j<size; j++)
+        {
+            if(arr[i] > arr[j])
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    printf("\nElements of array in ascending order: ");
+    for(i=0; i<size; i++)
+    {
+        printf("%d\t", arr[i]);
+    }
+ 
+    return 0;
 }
-
