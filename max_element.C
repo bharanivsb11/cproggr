db9e024b2d0ba54814720ce3bc1eee6f597c493d
bharanivsb11 <35220778+bharanivsb11@users.noreply.github.com>
2018-01-31 16:19:09 +0530

    max_element.C

--- max_element.C
+++ max_element.C
@@ -1,21 +1,21 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
 int main()
 {
-int i,n;
-int arr[25];
-printf("enter the number of element",n);
-scanf("%d\n",&n);
-for(i=0;i<n;++i)
-{
-printf("enter the number",arr[i+1]);
-scanf("%d",&arr[i]);
-}
-for(i=0;i<n;++i)
-{
-if(arr[0]<arr[i])
-arr[0]=arr[i];
-}
-printf("largest number is %d",&a[0]);
-return 0;
+    int i, n;
+    int arr[100];
+    printf("Enter total number of elements: ");
+    scanf("%d", &n);
+    printf("\n");
+    for(i = 0; i < n; ++i)
+    {
+       printf("Enter Number %d: ", i+1);
+       scanf("%d", &arr[i]);
+    }
+    for(i = 1; i < n; ++i)
+    {
+       if(arr[0] < arr[i])
+           arr[0] = arr[i];
+    }
+    printf("largest element = %d", arr[0]);
+    return 0;
 }
