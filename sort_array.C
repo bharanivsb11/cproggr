91b589144ce86ea495eae96722db9cc06040e706
bharanivsb11 <35220778+bharanivsb11@users.noreply.github.com>
2018-01-31 16:40:53 +0530

    sort_array.C

---
+++ sort_array.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,j,temp;
+int size;
+int arr[25];
+printf("enter the size of array");
+scanf("%d",&size);
+for(i=0;i<size;i++)
+{
+scanf("%d",&arr[i]);
+}
+
