4a375bb36ed6604bebf085079c46df66e9a1829c
bharanivsb11 <35220778+bharanivsb11@users.noreply.github.com>
2018-01-31 16:04:15 +0530

    min_element.C

--- min_element.C
+++ min_element.C
@@ -1,17 +1,16 @@
 #include<stdio.h>
-#include<conio.h>
 int main()
 {
 int i,n;
 int arr[20];
 printf("enter the number of elements",n);
 scanf("%d",&n);
-for(i=0;i<=n;++i)
+for(i=0;i<n;++i)
 {
 printf("enter the number",i+1);
 scanf("%d",&arr[i]);
 }
-for(i=0;i<=n;++i)
+for(i=0;i<n;++i)
 {
 if(arr[0]>arr[i])
 arr[0]=arr[i];
